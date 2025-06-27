dc63d069b06529f443b8621808536b3782d2dc91
Fabricio Schiavon Kolberg <gretgor42@gmail.com>
2025-06-27 18:35:35 -0300

    feat(retrieve_moves.py): start making the move and learnset retrieval function

--- retrieve_moves.py
+++ retrieve_moves.py
@@ -1,8 +1,22 @@
 import sys, os, requests, re
 from bs4 import BeautifulSoup
+from retrieve_pokemon import turn_links_into_text
 from pandas import read_html
 import pandas as pd
 from io import StringIO
+
+""" To recall: Table IDs
+
+0 - garbage
+1 - name
+2 - garbage
+3 - description, type, category, pp, power, accuracy, battle effect, 
+    secondary effect, contest type, contest effect, contest points (little number of hearts which is gonna be SO ANNOYING),
+    TM (if any), priority (quick attack and shit), target (one, multiple, all including allies, etc), affected by bright powder, 
+    affected by kings rock, makes contact
+4 and onwards - learnsets
+
+"""
 
 class MoveTable:
     att_list = ["name", "type", "category", "PP", "power", "accuracy", 
@@ -10,10 +24,10 @@ class MoveTable:
                 "contest_effect", "contest_points", "TM", "priority",
                 "target", "bright_powder", "kings_rock", "contact"]
     def __init__(self):
-        move_list = []
+        self.move_list = []
 
     def add_move(self, move):
-        move_list.append(move)
+        self.move_list.append(move)
 
     def save_move_data(self, outfile):
         with open(outfile,"w") as file:
@@ -33,10 +47,10 @@ class MoveTable:
 class LearnTable:
     att_list = ["nat_dex_id", "move", "level", "method"]
     def __init__(self):
-        learn_list = []
+        self.learn_list = []
 
     def add_learn(self, learn):
-        learn_list.append(learn)
+        self.learn_list.append(learn)
 
     def save_learn_data(self, outfile):
         with open(outfile,"w") as file:
@@ -52,6 +66,13 @@ class LearnTable:
                     else:
                         file.write(f"{learn[key]}; ")
                 file.write("\n")
+
+def check_move_info(dataframe):
+    outdict = {}
+    return outdict
+
+def check_learnset(dataframe, learn_table, move_name, method):
+    print(f"move name {move_name}, learn method '{method}'", file=sys.stderr)
 
 if __name__ == '__main__':
     with open("attackdex_main_page.html", "r", encoding="latin1") as file:
@@ -100,6 +121,17 @@ if __name__ == '__main__':
         # corresponds to)
         learn_types = [line.split('By')[1].split('<')[0][1:].split(':')[0] 
                        for line in full_data if "That Learn" in line]
+
+        text_soup = turn_links_into_text('\n'.join(full_data))
+        dataframes = read_html(StringIO(str(text_soup)))
         
-            
+        # first, retrieve move information and add to move table
+        move_info = {"name" : dataframes[1][0].iloc[0]}
+        move_info.update(check_move_info(dataframes[3]))
+        move_table.add_move(move_info)
+
+        # second, retrieve learnsets from moves
+        for i in range(len(learn_types)):
+            check_learnset(dataframes[i+4], learn_table, move_info['name'], learn_types[i])
+
         
